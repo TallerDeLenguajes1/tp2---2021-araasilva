3e311fe78b17532e4eb512f22a18947fb3219779
Araceli <araasilva96@gmail.com>
2021-04-15 15:26:47 -0300

    primer punto

---
+++ tp2_1_1.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#define N 4
+#define M 5
+int f,c;
+double mt[N][M];
+
+int main(){
+    for(f = 0;f<N; f++){
+        for(f = 0;f<N; f++){
+            printf("%lf ", mt[f][c]);
+        }
+        printf("\n");
+    }
+    getchar();
+    return 0;
+}
+
